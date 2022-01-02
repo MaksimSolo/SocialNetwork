5b953c345408d6098048d022f3c6b3a8e59a415e
MaksimSolo <taezhnyi@gmail.com>
2022-01-02 16:17:42 +0300

    #0-13 parts, incl git-repo, up to css-modules

---
+++ src/Components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://www.logodesign.net/images/nature-logo.png"/>
+        </header>
+    );
+};
+
---
+++ src/Components/Profile.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img
+                    src='https://media.istockphoto.com/photos/concept-of-an-open-magic-book-open-pages-with-water-and-land-and-picture-id1279460648?b=1&k=20&m=1279460648&s=170667a&w=0&h=uZa830sWo8hlFN0Y7FnQ14giNC0Z2EBNuTMuNJeJhQg='/>
+            </div>
+            <div>
+                Avatar + Description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        Post1
+                    </div>
+                    <div>
+                        Post2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
