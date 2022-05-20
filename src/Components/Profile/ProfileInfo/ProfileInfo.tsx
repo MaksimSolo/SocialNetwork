7b82dc2f4d22d9e2fe28d7f4a5175e53e9b9b8db
MaksimSolo <taezhnyi@gmail.com>
2022-05-20 22:53:14 +0300

    #68 episode for deploy

--- src/Components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/Components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -14,10 +14,6 @@ export const ProfileInfo = (props: ProfileInfoType) => {
     }
     return (
         <div>
-            <div>
-                <img
-                    src='https://media.istockphoto.com/photos/concept-of-an-open-magic-book-open-pages-with-water-and-land-and-picture-id1279460648?b=1&k=20&m=1279460648&s=170667a&w=0&h=uZa830sWo8hlFN0Y7FnQ14giNC0Z2EBNuTMuNJeJhQg='/>
-            </div>
             <div className={classes.descriptionBlock}>
                 <img src={props.profile.photos.small}/>
                 <div>ID: {props.profile.userId}</div>
