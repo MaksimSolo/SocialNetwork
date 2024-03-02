20aa49795854d4f81c6f619aeee3b6a79b9be6a0
MaksimSolo <taezhnyi@gmail.com>
2024-03-02 17:02:50 +0300

    fix: #96 switching to own profile when 'Profile' clicked during viewing someone else's profile

--- src/Components/Profile/ProfileContainer.tsx
+++ src/Components/Profile/ProfileContainer.tsx
@@ -4,65 +4,80 @@ import {compose} from "redux";
 import {withRouter} from "../../customWithRouter";
 import {withAuthRedirectComponent} from "../../highOrderComp/withAuthRedirectComponent";
 import {
-    getUserProfileTC,
-    getUserStatusTC,
-    updateUserStatusTC,
-    UsersProfilePropsType
+  getUserProfileTC,
+  getUserStatusTC,
+  updateUserStatusTC,
+  UsersProfilePropsType
 } from "../../redux/reducers/profile-data-reducer";
 import {AppStateType} from "../../redux/redux-store";
 import {
-    selectAuthUserIdMain,
-    selectProfileStatusMain,
-    selectUserProfileDataMain
+  selectAuthUserIdMain,
+  selectProfileStatusMain,
+  selectUserProfileDataMain
 } from "../../redux/selectors/profileSelectors";
 import {Profile} from "./Profile";
 
 
 type ProfileContainerType = {
-    authUserId: number
-    profile: UsersProfilePropsType | null
-    router: { location: any, navigation: any, params: any }
-    getUserProfileTC: (userID: number) => void
-    status: string
-    getUserStatusTC: (userID: number) => void
-    updateUserStatusTC: (newStatus: string,) => void
+  authUserId: number
+  profile: UsersProfilePropsType | null
+  router: { location: any, navigation: any, params: any }
+  getUserProfileTC: (userID: number) => void
+  status: string
+  getUserStatusTC: (userID: number) => void
+  updateUserStatusTC: (newStatus: string,) => void
 }
 
 
 class ProfileContainer extends React.Component<ProfileContainerType, AppStateType> {
 
-    componentDidMount() {
+  protected updateProfile(): void {
+    let userId = this.props.router.params['*'] ?? '';
 
-        let userID = this.props.router.params['*'] ?? '';
-        if (userID === '') {
-            userID = this.props.authUserId
-            if (!userID) {
-                this.props.router.navigation('/login')
-            }
-        }
-        this.props.getUserProfileTC(userID)
-        this.props.getUserStatusTC(userID)
+    if (!userId && !this.props.authUserId) {
+      this.props.router.navigation('/login')
     }
 
-
-    render = () => {
-
-        return (<><Profile {...this.props} /></>);
+    if (!userId) {
+      userId = this.props.authUserId
     }
+
+    this.props.getUserProfileTC(userId)
+    this.props.getUserStatusTC(userId)
+  }
+
+  componentDidMount() {
+    this.updateProfile()
+  }
+
+  componentDidUpdate(prevProps: Readonly<ProfileContainerType>) {
+    let currentUserId = this.props.router.params['*'] ?? '';
+    let prevUserId = prevProps.router.params['*'] ?? '';
+
+    if (currentUserId !== prevUserId) {
+      this.updateProfile()
+    }
+  }
+
+
+  render = () => {
+
+    return (<><Profile {...this.props} /></>);
+  }
 }
 
 const mapStateToProps = (state: AppStateType) => {
 
-    return ({
-        profile: selectUserProfileDataMain(state),
-        authUserId: selectAuthUserIdMain(state),
-        status: selectProfileStatusMain(state),
-    })
+  return ({
+    profile: selectUserProfileDataMain(state),
+    authUserId: selectAuthUserIdMain(state),
+    status: selectProfileStatusMain(state),
+  })
 }
 export const ProfileContainerWithRouter = compose<React.ComponentType>(
-    connect(mapStateToProps, {getUserProfileTC, getUserStatusTC, updateUserStatusTC}),
-    withAuthRedirectComponent,
-    withRouter
+  connect(mapStateToProps, {getUserProfileTC, getUserStatusTC, updateUserStatusTC}),
+  withAuthRedirectComponent,
+  withRouter
 )(ProfileContainer)
 
 
